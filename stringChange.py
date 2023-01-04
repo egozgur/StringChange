5a144fe02f70c86428012fecaa304025cf59dee4
egemen ozgur <100779439+egozgur@users.noreply.github.com>
2023-01-04 14:29:39 +0300

    Delete stringChange.py

--- stringChange.py
+++
@@ -1,8 +0,0 @@
-import fileinput
-with fileinput.FileInput('line_wmkl_sonuc.xml', inplace=True) as file: #girdi olucak dosyanın tanımlandığı kısım  
-    def my_function():
-        for line in file:
-          idas =  print('a',line.replace('LineStringSegment', 'LineString'))  #'LineStringSegment'= değiştirilmesi istenen kelime . 'LineString'=yerine gelmesi istenen kelime  
-        return idas
-my_function()
-
